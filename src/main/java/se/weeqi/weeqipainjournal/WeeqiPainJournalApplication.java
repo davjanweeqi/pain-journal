cf21da1bc4f049c9271ed90772e99f4b1fdeb4c0
David Jansson <david.jansson@weeqi.se>
2019-05-02 14:09:40 +0200

    First Commit of Pain Journal (name TBD)

---
+++ src/main/java/se/weeqi/weeqipainjournal/WeeqiPainJournalApplication.java
@@ -0,0 +1,13 @@
+package se.weeqi.weeqipainjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeqiPainJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeqiPainJournalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/weeqi/weeqipainjournal/WeeqiPainJournalApplicationTests.java
@@ -0,0 +1,16 @@
+package se.weeqi.weeqipainjournal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeeqiPainJournalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
